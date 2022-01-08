05987ba9f799f814413c349c58fcd6414533e51e
Nyan Thar Myint <nyanthar.ktu.it@gmail.com>
2022-01-08 23:10:23 +0630

    example route

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+  response.send("Monkeys are here!");
+});
+
+app.listen("8080", () => {
+  console.log("App listening at http://localhost:8080");
+});
